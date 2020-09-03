b5b8107a70df875906f1b36522ef45b601ab3e8a
Martin Nesse <mbnesse@gmail.com>
2020-09-03 15:48:47 +0200

    ny commit, nye filer

---
+++ vector.py
@@ -0,0 +1,16 @@
+
+
+class Vector3D:
+    def __init__(self, x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def __str__(self):
+        return f"({self.x}, {self.y}, {self.z})"
+
+
+if __name__ == "__main__":
+    v = Vector3D(1,1,1)
+    print(v)
+
