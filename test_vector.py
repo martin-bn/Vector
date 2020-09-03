ab704d810b8093c776e3f58e3d01561d10210f4c
Martin Nesse <mbnesse@gmail.com>
2020-09-03 21:48:19 +0200

    snart ferdig med forelesning nå

--- test_vector.py
+++ test_vector.py
@@ -1,4 +1,5 @@
 from vector import Vector3D
+import pytest
 
 
 def test_vector_str():
@@ -22,3 +23,28 @@ def test_vector_add_eq():
     u = Vector3D(1,1,1)
     w = u + v
     assert w == Vector3D(2,3,4)
+
+def test_vector_add_int():
+    v = Vector3D(1,2,3)
+    u = 1
+    w = v + u
+    assert w == Vector3D(2,3,4)
+
+def test_vector_add_int_right():
+    v = Vector3D(1,2,3)
+    u = 1
+    w = u + v
+    assert w == Vector3D(2,3,4)
+
+def test_vector_add_string_raises_TypeError():
+    v = Vector3D(1,2,3)
+    u = "Hello"
+    with pytest.raises(TypeError):
+        u + v
+
+def test_vector_dot_product():
+    v = Vector3D(1,2,3)
+    u = Vector3D(1,1,1)
+    w = u.dot(v)
+    assert isinstance(w, (int, float))
+    assert abs(w - 6) < 1e-12
--- vector.py
+++ vector.py
@@ -12,18 +12,31 @@ class Vector3D:
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z})"
+    
+    def __radd__(self, other):
+        return self.__add__(other)
 
+    
     def __add__(self, other):
         """ Add two vectors """
-        print("self = ", self)
-        print("other = ", other)
-        return Vector3D(self.x + other.x, self.y + other.y, + self.z + other.z)
+        #pdb.set_trace()
+        if isinstance(other, (int, float)):
+            return Vector3D(self.x + other, self.y + other, self.z + other)
+        elif isinstance(other, Vector3D):
+            return Vector3D(self.x + other.x, self.y + other.y, + self.z + other.z)
+        else:
+            raise TypeError(f"cannot add vector3d to object of type {type(other)}")
+        
         
     def __eq__(self, other):
         """ Return True if self == other,
         otherwise return False """
         #pdb.set_trace()
         return self.x == other.x and self.y == other.y and self.z==other.z
+
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
 
 if __name__ == "__main__":
     v = Vector3D(1,4,2)
