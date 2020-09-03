1b1ca6930f428f9120eeeb5acd73b1dd8ff9fe2e
Martin Nesse <mbnesse@gmail.com>
2020-09-03 18:09:54 +0200

    repr-metode for vector class

--- test_vector.py
+++ test_vector.py
@@ -5,8 +5,9 @@ def test_vector_str():
     v = Vector3D(1,2,3)
     assert str(v) == "(1, 2, 3)"
 
-
-
+def test_vector_repr():
+    v = Vector3D(1,2,3)
+    assert repr(v) == "Vector3D(1, 2, 3)"
 
 
 
--- vector.py
+++ vector.py
@@ -10,7 +10,10 @@ class Vector3D:
         return f"({self.x}, {self.y}, {self.z})"
 
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z})"
+
 if __name__ == "__main__":
     v = Vector3D(1,1,1)
-    print(v)
+    print([v])
 
